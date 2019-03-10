0712fc176f42d8626a85493469da6b4db4457d9a
Raineer <delaritaraineer81@gmail.com>
2019-03-10 20:12:58 +0800

    Adding npm install morgan: logger : GET /orders 200

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+// Handle incoming GET requests to /orders
 router.get('/', (req, res, nex) => {
     res.status(200).json({
         message: 'Orders were fetched'
