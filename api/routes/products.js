331c12f99d21ea274c956ae5dd3594076765293f
Raineer <delaritaraineer81@gmail.com>
2019-03-10 18:55:07 +0800

    get productID

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,14 +3,28 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handling GET requests to / products'
+        message: 'Handling GET requests to /products'
     });
 });
 
 router.post('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handling POST requests to / products'
+        message: 'Handling POST requests to /products'
     });
 });
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if ( id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+})
 
 module.exports = router;
