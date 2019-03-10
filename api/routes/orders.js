5590e27121ad7f34363dc78056f999b43527f34d
Raineer <delaritaraineer81@gmail.com>
2019-03-10 21:55:11 +0800

    const order and product . order property to const order

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,26 +3,31 @@ const router = express.Router();
 
 
 // Handle incoming GET requests to /orders
-router.get('/', (req, res, nex) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Orders were fetched'
     });
 });
 
-router.post('/', (req, res, nex) => {
+router.post('/', (req, res, next) => {
+    const order = { //javascript object
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(201).json({
-        message: 'Orders was created'
+        message: 'Orders was created',
+        order: order // order property is from const order
     });
 });
 
-router.get('/:orderId', (req, res, nex) => {
+router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order details',
         orderId: req.params.orderId
     });
 });
 
-router.delete('/:orderId', (req, res, nex) => {
+router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order deleted',
         orderId: req.params.orderId
