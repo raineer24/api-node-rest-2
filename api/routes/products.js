a7c2f274ed144d76e8d6aa514c47da4d9b17939d
Raineer <delaritaraineer81@gmail.com>
2019-03-11 15:52:09 +0800

    productId doesn't exist code block

--- api/routes/products.js
+++ api/routes/products.js
@@ -37,7 +37,13 @@ router.get('/:productId', (req, res, next) => {
      .exec()
      .then(doc => {
          console.log("From database", doc);
-         res.status(200).json(doc); 
+         if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({ 
+                message: 'No valid entry found for provided ID'
+            });
+        }
      })
      .catch(err => {
         console.log(err);
