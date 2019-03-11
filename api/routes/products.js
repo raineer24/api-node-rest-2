65a226646ca0f9d7a5681f19a286a1c3a85b547d
Raineer <delaritaraineer81@gmail.com>
2019-03-11 16:40:56 +0800

    refactor get request

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,10 +4,23 @@ const mongoose = require('mongoose');
 
 const Product = require('../models/product'); // product model
 
+// router.get('/', (req, res, next) => {
+//     res.status(200).json({
+//         message: 'Handling GET requests to /products'
+//     });
+// });
+
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    });
+    Product.find()
+     .exec()
+     .then(docs => {
+         console.log(docs);
+         res.status(200).json(docs);
+     })
+     .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+     });
 });
 
 router.post('/', (req, res, next) => {
